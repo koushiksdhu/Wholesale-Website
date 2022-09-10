88aef9fb83b11c3be053ba3b39903b8c45d11875
Koushik Sadhu <kkoushikssadhu@gmail.com>
2022-09-10 15:54:48 +0530

    Login and Sigup pages added

--- Wholesale-Website/README.md
+++
@@ -1,2 +0,0 @@
-# Wholesale-Website
-This is a Wholesale website we are creating forHackazard Hack 2022.
--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+//script of Login and Signup Page
+
+const container1 = document.querySelector(".container1"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "text";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    })
+                }else{
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    })
+                }
+            }) 
+        })
+    })
+
+    // js code to appear signup and login form
+    signUp.addEventListener("click", ( )=>{
+        container1.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container1.classList.remove("active");
+    });
+
 var menuItems = document.getElementById("Menu-items")
 
 menuItems.style.maxHeight = "0px";
@@ -10,3 +46,5 @@ function menutoggle() {
         menuItems.style.maxHeight = "0px";
     }
 }
+
+

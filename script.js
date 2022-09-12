a4df4d0c9bb40ffefe65b4c5508c717b42c92812
Saptarshi Banerjee <90561422+TheSaptarshiBanerjee@users.noreply.github.com>
2022-09-12 23:45:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,72 @@ function menutoggle() {
         menuItems.style.maxHeight = "0px";
     }
 }
+//script of Login and Signup Page
+
+const container1 = document.querySelector(".container1"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "text";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    })
+                }else{
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    })
+                }
+            }) 
+        })
+    })
+
+    // js code to appear signup and login form
+    signUp.addEventListener("click", ( )=>{
+        container1.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container1.classList.remove("active");
+    });
+
+var menuItems = document.getElementById("Menu-items")
+
+menuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+    if (menuItems.style.maxHeight == "0px") {
+        menuItems.style.maxHeight = "200px";
+    }
+    else {
+        menuItems.style.maxHeight = "0px";
+    }
+}
+
+var noti = document.querySelector('h1');
+	var select = document.querySelector('.select');
+	var button = document.getElementsByTagName('button');
+	for(var but of button){
+		but.addEventListener('click', (e)=>{
+			var add = Number(noti.getAttribute('data-count') || 0);
+			noti.setAttribute('data-count', add +1);
+			noti.classList.add('zero');
+        })
+    }
+    var parent = e.target.parentNode;
+			var clone = parent.cloneNode(true);
+			select.appendChild(clone);
+			clone.lastElementChild.innerText = "Buy-now";
+			
+			if (clone) {
+				noti.onclick = ()=>{
+					select.classList.toggle('display');
+				}	
+			}
